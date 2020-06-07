dd06b11a5f494908ab12e4d46699615a9c43bd2d
MichelGoncalves5 <goncalvesmichel5@gmail.com>
2020-06-07 04:11:38 -0300

    Quiz structure

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,76 @@
-function start() {
-    alert("Começou");
+const question = document.querySelector('#question');
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptinAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+    question: "Pergunta 1",
+    choice1: "resposta 1",
+    choice2: "resposta 2",
+    answer: 1
+},
+{
+    question: "Pergunta 2",
+    choice1: "resposta a",
+    choice2: "resposta b",
+    answer: 1
+}, 
+{
+    question: "Pergunta 3",
+    choice1: "resposta 1",
+    choice2: "resposta 2",
+    answer: 1
+}
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+const startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+
+    getNewQuestion();
 }
 
-document.getElementById("pg1").innerHTML = soma (5,7);
+const getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('end.html');
+    }
 
-function soma (x,y){
-    return x + y;
-}
+    questionCounter++;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion[`choice${number}`];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach( choice => {
+    choice.addEventListener("click", e => {
+        if(!acceptingAnswers) return;
+
+        acceptinAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        
+        getNewQuestion();
+    });
+});
+
+startGame();
