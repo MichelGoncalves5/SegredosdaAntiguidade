8c05b95945ea8e0c8bba9457929d5e99abf8a611
MichelGoncalves5 <goncalvesmichel5@gmail.com>
2020-06-15 10:27:06 -0300

    Modal Config
    Some ajustments in the endgame modal code. Now to the styles.

--- js/end.js
+++ js/end.js
@@ -2,19 +2,10 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-finalScore.innerText = mostRecentScore;
 
+finalScore.innerText = mostRecentScore +"%";
 
-
-username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.value;
-});
-
-saveHighScore = e => {
-    console.log("clicked the save btn");
-    e.preventDefault();
-}
-
+//for adding user name on form for modal
 
 document.getElementById('btn-hi').addEventListener('click',saveName);
 
@@ -22,3 +13,26 @@ function saveName(input){
 var input = document.getElementById('userInput').value;
 hi.textContent = input;
 };
+
+//for social media sharing
+
+$('a.share').click(function(e){
+  e.preventDefault();
+  const $link   = $(this);
+  const href    = $link.attr('href');
+  const network = $link.attr('data-network');
+  
+  const networks = {
+      facebook : { width : 600, height : 300 },
+      twitter  : { width : 600, height : 254 },
+      google   : { width : 515, height : 490 },
+      linkedin : { width : 600, height : 473 }
+  };
+  
+  const popup = function(network){
+      var options = 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,';
+      window.open(href, '', options+'height='+networks[network].height+',width='+networks[network].width);
+  }
+  
+  popup(network);
+  });
