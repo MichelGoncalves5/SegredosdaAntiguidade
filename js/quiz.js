3b0facef5951852f08af102a8c876b23b80d2576
MichelGoncalves5 <goncalvesmichel5@gmail.com>
2020-06-15 16:49:27 -0300

    Stylized Quiz
    Finished styling the quiz, now onto putting it on index.

--- js/quiz.js
+++ js/quiz.js
@@ -14,64 +14,88 @@ let availableQuestions = [];
 
 let questions = [
     {
-    question: "Pergunta 1",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
-    answer: 1
+    question: "Como ficaram conhecidos os responsáveis por patrocinar as obras de arte no renascimento?",
+    choice1: "Padrinhos",
+    choice2: "Mecenas",
+    answer: 2
 },
 {
-    question: "Pergunta 2",
-    choice1: "resposta a",
-    choice2: "resposta b",
-    answer: 1
+    question: "Nome do gênio que projetou a cúpula do Duomo de Florença, e resgatou a técnica do Ponto de Fuga nas pintuas:",
+    choice1: "Leonardo da Vinci",
+    choice2: "Filippo Brunelleschi",
+    answer: 2
 }, 
 {
-    question: "Pergunta 3",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "A Divina Comédia, de Dante Alighieri, foi tão importante que acabou consolidando o dialeto falado na região da toscana como o idioma oficial italiano.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 4",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "Em 1513, após marchar contra  Prato, localidade de sua cidade natal Florença, e matar milhares de pessoas, Giovanni de Médici retornou do exílio e foi consagrado como papa Leão X.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 5",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "Apesar da rivalidade nas artes,Michaelangelo considerava Leonardo da Vinci um grande artista, com uma visão realmente avançada para sua época.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 6",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "Leonardo da Vinci podia dissecar cadáveres sem infringir a lei, graças a uma permissão especial que recebeu como reconhecimento de sua genialidade como artista, inventor e cientista.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 7",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "O David de Michaelangelo, escultura mais famosa do renascimento, foi uma homenagem do artista à família Médici, que o abrigou em sua casa e patrocinou muitas de suas obras.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
+    answer: 2
+},
+{
+    question: "Um dos motivos que o renascimento da Inglaterra começou depois do italiano foi seu envolvimento na Guerra dos Cem Anos",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 8",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "Graças à Shakespeare, os teatros ingleses passaram a aceitar mulheres atuando nos palcos.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
+    answer: 2
+},
+{
+    question: "Na espanha, o período do renascimento ficou manchado por um Grande Inquisidor a quem são atribuídas mais de 2.000 execuções, principalmente de mulheres e judeus.",
+    choice1: "Miguel de Cervantes",
+    choice2: "Tomás de Torquemada",
+    answer: 2
+},
+{
+    question: "A conquista de uma importante cidade pelos Otomanos, em 1453, promoveu o fluxo de livros e sábios para a Italia, acelerando o renascimento.",
+    choice1: "Jerusalém",
+    choice2: "Constantinopla",
+    answer: 2
+},
+{
+    question: "O Malleus Maleficarum, livro que instruía como identificar e torturar bruxas, foi aprovado por uma bula do Papa Inocêncio VIII.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 9",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
+    question: "Ao ser convidado pelo Papa Julio II para pintar a Capela Cistina, Michaelangelo afirmou estar sendo castigado, para que não pudesse trabalhar em suas esculturas.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
     answer: 1
 },
 {
-    question: "Pergunta 10",
-    choice1: "resposta 1",
-    choice2: "resposta 2",
-    answer: 1
+    question: "A pintura dos 460 m2 da Capela Cistina, na verdade, foi feita pelos ajudantes de Michaelangelo, que planejou vistoriou todos os detalhes.",
+    choice1: "Verdadeiro",
+    choice2: "Falso",
+    answer: 2
 }
 ];
 
